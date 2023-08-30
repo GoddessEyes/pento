0d34577445a4ae7b13a0c2a24315fb256f2abaef
goddesseyes <goddesseyes@protonmail.com>
2023-08-30 23:21:53 +0300

    schemaless changesets

--- lib/pento_web/router.ex
+++ lib/pento_web/router.ex
@@ -45,12 +45,15 @@ defmodule PentoWeb.Router do
   scope "/", PentoWeb do
     pipe_through([:browser, :require_authenticated_user])
 
+    live_session :default, on_mount: [{PentoWeb.UserAuth, :ensure_authenticated}] do
+      live("/guess", WrongLive)
+      live("/promo", PromoLive)
+    end
+
     live_session :require_authenticated_user,
       on_mount: [{PentoWeb.UserAuth, :ensure_authenticated}] do
       live("/users/settings", UserSettingsLive, :edit)
       live("/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email)
-
-      live("/guess", WrongLive)
 
       # Products
       live("/products", ProductLive.Index, :index)
