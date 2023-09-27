b0128f229683078f7db09826b43c28d0f32a322e
goddesseyes <goddesseyes@protonmail.com>
2023-09-28 00:12:33 +0300

    Start survey-show

--- lib/pento_web/live/survey_live.ex
+++ lib/pento_web/live/survey_live.ex
@@ -2,7 +2,15 @@ defmodule PentoWeb.SurveyLive do
   @moduledoc false
   use PentoWeb, :live_view
 
+  alias Pento.Survey
+
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok,
+     socket
+     |> assign_demographic()}
+  end
+
+  defp assign_demographic(%{assigns: %{current_user: current_user}} = socket) do
+    assign(socket, :demographic, Survey.get_demographic_by_user(current_user))
   end
 end
--- lib/pento_web/live/survey_live/component.ex
+++ lib/pento_web/live/survey_live/component.ex
@@ -8,18 +8,12 @@ defmodule PentoWeb.SurveyLive.Component do
 
   def hero(assigns) do
     ~H"""
-    <h1 >
+    <h1>
       <%= @content %>
     </h1>
     <h3>
       <%= render_slot(@inner_block) %>
     </h3>
-
-    <pre>
-      <%= inspect(assigns) %>
-      <% %{ inner_block: [%{inner_block: block_fn}]} = assigns %>
-      <%= inspect(block_fn.(assigns.__changed__, assigns)) %>
-    </pre>
     """
   end
 end
