f50e2f232d5fbe30cadbe480831f1dff26b79e4b
goddesseyes <goddesseyes@protonmail.com>
2023-10-02 19:54:11 +0300

    Logic for save demographic survey

--- lib/pento_web/live/demographic_live/form.ex
+++ lib/pento_web/live/demographic_live/form.ex
@@ -15,6 +15,26 @@ defmodule PentoWeb.DemographicLive.Form do
     }
   end
 
+  def handle_event("save", %{"demographic" => demographic_params}, socket) do
+    params = params_with_user_id(demographic_params, socket)
+    {:noreply, save_demographic(socket, params)}
+  end
+
+  defp save_demographic(socket, demographic_params) do
+    case Survey.create_demographic(demographic_params) do
+      {:ok, demographic} ->
+        send(self(), {:created_demographic, demographic})
+        socket
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        assign(socket, changeset: changeset)
+    end
+  end
+
+  def params_with_user_id(params, %{assigns: %{current_user: current_user}}) do
+    Map.put(params, "user_id", current_user.id)
+  end
+
   defp assign_demographic(%{assigns: %{current_user: current_user}} = socket) do
     assign(socket, :demographic, %Demographic{user_id: current_user.id})
   end
--- lib/pento_web/live/survey_live.ex
+++ lib/pento_web/live/survey_live.ex
@@ -10,6 +10,16 @@ defmodule PentoWeb.SurveyLive do
      |> assign_demographic()}
   end
 
+  def handle_info({:created_demographic, demographic}, socket) do
+    {:noreply, handle_demographic_created(demographic, socket)}
+  end
+
+  def handle_demographic_created(demographic, socket) do
+    socket
+    |> put_flash(:info, "Demographic created successfully")
+    |> assign(:demographic, demographic)
+  end
+
   defp assign_demographic(%{assigns: %{current_user: current_user}} = socket) do
     assign(socket, :demographic, Survey.get_demographic_by_user(current_user))
   end
